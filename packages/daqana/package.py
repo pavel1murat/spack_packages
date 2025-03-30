2b7164e427f8b9d7ac697bed8aca928daa113b22
Mu2e <mu2etrk@mu2edaq22.fnal.gov>
2025-03-30 09:10:07 -0500

    - add daqana

--- packages/daqana/package.py
+++ packages/daqana/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
+import os
 
 
 def sanitize_environments(env, *vars):
@@ -15,8 +16,8 @@ class Daqana(CMakePackage):
     """FIXME: Put a proper description of your package here."""
 
     homepage = "https://github.com/pavel1murat/daqana/"
-    git = "https://github.com/pavel1murat/daqana.git"
-    url = "https://github.com/pavel1murat/daqana/archive/refs/tags/v1_04_00.tar.gz"
+    git = "https://github.com/pavel1murat/frontends.git"
+    url = "https://github.com/pavel1murat/frontends/archive/refs/tags/v1_04_00.tar.gz"
 
     maintainers("pavel1murat")
 
@@ -24,11 +25,11 @@ class Daqana(CMakePackage):
 
     version("main", branch="main", get_full_repo=True)
 #    version("v3_01_00", commit="e7b7abb733e00e8a97f31f02f87746fb29c4949e")
-#    version("v3_01_00", branch="main", get_full_repo=True)
-#    version("v3_00_00", branch="main", get_full_repo=True)
+    version("v3_01_00", branch="main", get_full_repo=True)
+    version("v3_00_00", branch="main", get_full_repo=True)
 
     def url_for_version(self, version):
-        url = "https://github.com/pavel1murat/daqana/archive/refs/tags/{0}.tar.gz"
+        url = "https://github.com/pavel1murat/frontends/archive/refs/tags/{0}.tar.gz"
         return url.format(version)
 
     variant(
@@ -39,13 +40,27 @@ class Daqana(CMakePackage):
         description="Use the specified C++ standard when building.",
     )
 
+    depends_on("cetmodules", type="build")
     depends_on("Offline")
 
     def cmake_args(self):
         args = [
             self.define_from_variant("CMAKE_CXX_STANDARD", "cxxstd"),
         ]
+        if os.path.exists("CMakePresets.cmake"):
+            args.extend(["--preset", "default"])
+        else:
+            self.define("artdaq_core_OLD_STYLE_CONFIG_VARS", True)
         return args
+
+    def setup_run_environment(self, env):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
 
     def setup_dependent_run_environment(self, env, dependent_spec):
         prefix = self.prefix
@@ -55,4 +70,3 @@ class Daqana(CMakePackage):
         env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
         # Cleaup.
         sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
-

27d70667c9e562a378e1ab09451ea61c3501a112
Pavel Murat <murat@fnal.gov>
2025-12-09 08:32:59 -0600

    - add grim recipe

---
+++ packages/grim/package.py
@@ -0,0 +1,67 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+import os
+
+
+def sanitize_environments(env, *vars):
+    for var in vars:
+        env.prune_duplicate_paths(var)
+        env.deprioritize_system_paths(var)
+        
+class Grim(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://github.com/pavel1murat/grim/"
+    git = "https://github.com/pavel1murat/grim.git"
+    url = "https://github.com/pavel1murat/grim/archive/refs/tags/v1_04_00.tar.gz"
+
+    maintainers("pavel1murat")
+
+    license("BSD")
+
+    version("main", branch="main", get_full_repo=True)
+    version("v3_00_00", branch="main", get_full_repo=True)
+
+    def url_for_version(self, version):
+        url = "https://github.com/pavel1murat/grim/archive/refs/tags/{0}.tar.gz"
+        return url.format(version)
+
+    variant(
+        "cxxstd",
+        default="20",
+        values=("14", "17", "20"),
+        multi=False,
+        description="Use the specified C++ standard when building.",
+    )
+
+    depends_on("cetmodules", type="build")
+    depends_on("trace")
+    depends_on("midas")
+
+    def cmake_args(self):
+        args = [
+            self.define_from_variant("CMAKE_CXX_STANDARD", "cxxstd"),
+        ]
+        return args
+
+    def setup_run_environment(self, env):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        # env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        # env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
+
+    def setup_dependent_run_environment(self, env, dependent_spec):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        # env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        # env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")

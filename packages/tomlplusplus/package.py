4c0cc24e3b7c824b5d21ad887bc1b9909ead1f8f
Pavel Murat <pavel.murat@gmail.com>
2026-02-13 22:27:05 -0600

    - add

---
+++ packages/tomlplusplus/package.py
@@ -0,0 +1,73 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+import os
+
+
+def sanitize_environments(env, *vars):
+    for var in vars:
+        env.prune_duplicate_paths(var)
+        env.deprioritize_system_paths(var)
+        
+class Tomlplusplus(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://github.com/pavel1murat/muon_racetrack/"
+    git = "https://github.com/marzer/tomlplusplus.git"
+    url = "https://github.com/marzer/tomlplusplus/archive/refs/tags/v1_01_00.tar.gz"
+
+    maintainers("x")
+
+    license("BSD")
+
+    version("master", branch="master", get_full_repo=True)
+#    version("v3_01_00", commit="e7b7abb733e00e8a97f31f02f87746fb29c4949e")
+    version("v3_01_00", branch="master", get_full_repo=True)
+    version("v3_00_00", branch="master", get_full_repo=True)
+
+    def url_for_version(self, version):
+        url = "https://github.com/marzer/tomlplusplus/archive/refs/tags/{0}.tar.gz"
+        return url.format(version)
+
+    variant(
+        "cxxstd",
+        default="20",
+        values=("14", "17", "20"),
+        multi=False,
+        description="Use the specified C++ standard when building.",
+    )
+
+#    depends_on("cetmodules", type="build")
+#    depends_on("Offline")
+#    depends_on("Stntuple")
+
+    def cmake_args(self):
+        args = [
+            self.define_from_variant("CMAKE_CXX_STANDARD", "cxxstd"),
+        ]
+        if os.path.exists("CMakePresets.cmake"):
+            args.extend(["--preset", "default"])
+        else:
+            self.define("artdaq_core_OLD_STYLE_CONFIG_VARS", True)
+        return args
+
+    def setup_run_environment(self, env):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
+
+    def setup_dependent_run_environment(self, env, dependent_spec):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
